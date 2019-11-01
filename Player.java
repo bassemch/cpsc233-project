4ac8e45c7ea30f5ecc79d8b9533503a90f36ca01
MatthewKamila <MatthewKamila@gmail.com>
2019-11-01 02:35:05 -0600

    Add files via upload

--- Player.java
+++ Player.java
@@ -2,13 +2,14 @@ package SpaceInvadersGame;
 
 import javafx.scene.paint.Color;
 
-public class Enemy extends Sprite {
-	Enemy(int x, int y, int w, int h, String type, Color color) {
+public class Player extends Sprite {
+
+	Player(int x, int y, int w, int h, String type, Color color) {
 		super(x, y, w, h, type, color);
 	
 	}
 	
-	Enemy(Enemy toCopy){
+	Player(Player toCopy){
 		super(toCopy.xpos,toCopy.ypos, toCopy.width, toCopy.height, toCopy.type, toCopy.color);
 	}
 }
--- Projectile.java
+++ Projectile.java
@@ -2,13 +2,14 @@ package SpaceInvadersGame;
 
 import javafx.scene.paint.Color;
 
-public class Enemy extends Sprite {
-	Enemy(int x, int y, int w, int h, String type, Color color) {
+public class Projectile extends Sprite {
+
+	Projectile(int x, int y, int w, int h, String type, Color color) {
 		super(x, y, w, h, type, color);
 	
 	}
 	
-	Enemy(Enemy toCopy){
+	Projectile(Projectile toCopy){
 		super(toCopy.xpos,toCopy.ypos, toCopy.width, toCopy.height, toCopy.type, toCopy.color);
 	}
 }
